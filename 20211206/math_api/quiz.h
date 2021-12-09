ae16aa0e762f53606b01b8bb30f6e6bb0d6be0d7
SperandiniGiovanni <94382877+SperandiniGiovanni@users.noreply.github.com>
2021-12-09 14:08:00 +0100

    Update quiz.h

--- 20211206/math_api/quiz.h
+++ 20211206/math_api/quiz.h
@@ -1,1 +1,7 @@
 
+#ifndef __QUIZ_H__ //se quiz.h non è definito
+#define __QUIZ_H__ //allora definisco quiz.h 
+
+int quiz(int x, int y); // definizione della funzione da chiamare e relative funzioni
+
+#endif //fine
